e9f3a6ad783cf592c95c70bfffc6d6cd0f1be69e
Rahmatul Ramadhani <rahmatulramadhani06@gmail.com>
2025-02-22 13:11:31 +0700

    repair sign in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,37 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, NotFoundException, Post, Request, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
 import { AuthGuard } from '@/guards/auth/auth.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { UsersService } from '@/users/users.service';
 import { EmployeesService } from '@/employees/employees.service';
+import { LibService } from '@/lib/lib.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
     private readonly usersService: UsersService,
-    private readonly employeeService: EmployeesService
+    private readonly employeeService: EmployeesService,
+    private readonly libService: LibService,
+    private readonly jwtService: JwtService
   ) {}
 
   @HttpCode(HttpStatus.OK)
   @Post("signIn")
   async signIn(@Body() signInDto: SignInDto) {
-    return this.authService.signIn(signInDto);
+    const user = await this.usersService.findOne(signInDto.np);
+    if (!user) {
+        throw new NotFoundException('User not found')
+    }
+    if (!this.libService.verifyhash(signInDto.password, user.password)) {
+        throw new UnauthorizedException('Wrong password')
+    }
+    const payload = { sub: user.employee._id }
+    return {
+        token: await this.jwtService.signAsync(payload)
+    };
   }
 
   @UseGuards(AuthGuard)
